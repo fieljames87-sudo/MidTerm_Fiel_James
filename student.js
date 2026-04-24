2b03e977fb8210b91655a3229dc50e89ef4920f2
fieljames87-sudo <fieljames87@gmail.com>
2026-04-24 09:17:20 +0800

    Add files via upload

---
+++ student.js
@@ -0,0 +1,14 @@
+// student.js
+
+// Variables
+let name = "James Fiel";
+let course = "BS Information Technology";
+let yearLevel = "2nd Year";
+let favoriteLanguage = "JavaScript";
+
+// Output
+console.log("=== Student Information ===");
+console.log("Name: " + name);
+console.log("Course: " + course);
+console.log("Year Level: " + yearLevel);
+console.log("Favorite Programming Language: " + favoriteLanguage);
